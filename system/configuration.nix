95681f72d203bb43a416de5e7b55b95b5e9636d9
Brock Morrison <brock@seintofficial.com>
2022-01-12 18:50:19 +0100

    Enable Docker. Add "docker" user group to brockm

--- system/configuration.nix
+++ system/configuration.nix
@@ -20,6 +20,8 @@
     options = "-d";
   };
 
+  virtualisation.docker.enable = true;
+
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
--- users/brockm/nixos.nix
+++ users/brockm/nixos.nix
@@ -3,7 +3,7 @@
 {
   users.users.brockm = {
     isNormalUser = true;
-    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+    extraGroups = [ "wheel" "docker" ]; # Enable ‘sudo’ for the user.
     shell = pkgs.zsh;
     # hashedPassword = "$6$5afzstoy9svt/Nmk$MdFB0xk07JYP7ME6EtAlTA0g5xssFYJePX.Hvc9YsneMUXQo8il77mUDRATdgvjtoYAuNvh16LJOFrKfCSxvv/";
     initialPassword = "Password";
